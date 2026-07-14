e9e852049f89b2ae25d73b28fe7ba31657eda8a5
discojohnson <discojohnson@gmail.com>
2026-07-14 16:21:30 -0500

    stripped out proxmox_provider.env.encrypted references, as it will need to be created ad hoc

--- scripts/02_proxmox_provider.sh
+++ scripts/02_proxmox_provider.sh
@@ -7,21 +7,18 @@ echo "Proxmox Provider"
 #######
 echo "Environment file"
 PROXMOX_ENV=${OMNI_ASSETS}/proxmox_provider.env.decrypted
-echo "..Copying template to ${PROXMOX_ENV}
+echo "..Copying template to ${PROXMOX_ENV}"
 cp ../assets/proxmox_provider.env.template ${PROXMOX_ENV}
 
-echo "..Checking if encrypted version already exists"
-if [ -e ../assets/proxmox_provider.env.encrypted ]; then
-  echo "....It does, so decrypt it. Assuming password was fine though"
-  sh decrypt.sh ../assets/proxmox_provider.env.encrypted ${PROXMOX_ENV}
-fi
+echo "..Sourcing ${OMNI_ASSETS}/proxmox_provider.env.decrypted"
+. ${OMNI_ASSETS}/proxmox_provider.env.decrypted
 
 #######
-####### config.yaml
+####### proxmox_provider.config.yaml
 #######
-echo "config.yaml"
+echo "proxmox_provider.config.yaml"
 PROXMOX_CONFIG_YAML=${OMNI_ASSETS}/proxmox_provider.config.yaml.decrypted
-echo "..Copying template to ${PROXMOX_ENV}
+echo "..Copying template to ${PROXMOX_CONFIG_YAML}"
 cp ../assets/proxmox_provider.config.yaml.template ${PROXMOX_CONFIG_YAML}
 
 echo "..Checking if encrypted version already exists"
@@ -30,6 +27,7 @@ if [ -e ../assets/proxmox_provider.config.yaml.encrypted ]; then
   sh decrypt.sh ../assets/proxmox_provider.config.yaml.encrypted ${PROXMOX_CONFIG_YAML}
 fi
 
-
+echo "Copying Docker Compose file to ${OMNI_ASSETS}/proxmox_provider.compose.yaml"
+cp ../assets/proxmox_provider.compose.yaml ${OMNI_ASSETS}/proxmox_provider.compose.yaml
 echo "Launching via docker compose"
-docker compose -f ${OMNI_ASSETS}/compose.yaml --env-file ${OMNI_ASSETS}/omni.env up -d
+docker compose -f ${OMNI_ASSETS}/proxmox_provider.compose.yaml up -d
