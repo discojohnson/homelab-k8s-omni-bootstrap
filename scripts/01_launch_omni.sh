3b11b938fb7dadfc4d01466d927b79ae2e627ab7
discojohnson <discojohnson@gmail.com>
2026-07-14 15:48:15 -0500

    minor change

--- scripts/01_launch_omni.sh
+++ scripts/01_launch_omni.sh
@@ -1,10 +1,15 @@
 export OMNI_VERSION=`wget -qO - "https://api.github.com/repos/siderolabs/omni/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 
-OMNI_ASSETS=~/omni
+export OMNI_ASSETS=~/omni
 OMNI_PRIVATE_ENV=${OMNI_ASSETS}/private.env.decrypted
 
 echo "Sourcing ${OMNI_PRIVATE_ENV}"
 . ${OMNI_PRIVATE_ENV}
 
+export OMNI_DOMAIN_NAME
+export OMNI_ACCOUNT_UUID
+export OMNI_WG_IP
+export OMNI_ADMIN_EMAIL
+
 echo "Launching via docker compose"
-docker compose --env-file ${OMNI_ASSETS}/omni.env up -d
+docker compose -f ${OMNI_ASSETS}/compose.yaml --env-file ${OMNI_ASSETS}/omni.env up -d
